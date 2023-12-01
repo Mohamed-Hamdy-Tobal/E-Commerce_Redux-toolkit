cdf9c0b03840cc2250b85db1dcd7a19cd5991768
Mohamed Tobal <mohamedtobal369@gmail.com>
2023-12-01 20:15:01 +0200

    UP

--- src/Components/FilteredProducts/FilteredProducts.jsx
+++ src/Components/FilteredProducts/FilteredProducts.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
+import { ProductCard } from "./ProductCard"
+import { Container } from "react-bootstrap"
 
 const FilteredProducts = () => {
 
@@ -9,17 +11,33 @@ const FilteredProducts = () => {
     console.log(filterProducts)
 
     return (
-        <div>
-            <div className="pt-3">
-                <div className="ps-2">
-                    <h1>{type}</h1>
-                    <div className="d-flex justify-content-center align-items-center gap-2">
-                        
+        <main className="products pb-5">
+            <Container>
+                <div className="pt-3">
+                    <div className="ps-2">
+                        <h1 className="p-3 products-title position-relative" style={{width: 'fit-content'}}>{type}</h1>
+                        <div className="d-flex justify-content-center align-items-center flex-wrap">
+                            {filterProducts.map((product, idx) => {
+                                return (
+                                    <div className="f p-3 card-product" key={idx}>
+                                        <ProductCard 
+                                        id={product.id} 
+                                        name={product.name} 
+                                        colors={product.color}
+                                        img={product.img}
+                                        price={product.price}
+                                        text={product.text}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </Container>
+        </main>
     )
 }
 
 export default FilteredProducts
+
+// 21
--- src/Components/FilteredProducts/ProductCard.jsx
+++ src/Components/FilteredProducts/ProductCard.jsx
@@ -1,16 +1,24 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 /* eslint-disable react/prop-types */
-export const ProductCard = ({name, img, text}) => {
+export const ProductCard = ({name, img, text, price, colors}) => {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={img}/>
+        <Card className='shadow-sm'>
+            <div className='main-img'>
+                <Card.Img variant="top" src={img} className='img-fluid'/>
+            </div>
             <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>{text}</Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+            <Card.Title className="fw-bold">{name}</Card.Title>
+            <Card.Text style={{color: '#777', fontSize: '14px', lineHeight: '1.4'}}>{text}</Card.Text>
             </Card.Body>
+            <Card.Footer className="d-flex justify-content-between align-items-center">
+                <small className="text-muted fw-bold">{price}$</small>
+                <div className="colors d-flex gap-1">
+                    {colors.map((color, idx) => {
+                        return(<i className='fab fa-marker rounded-circle color-icon' key={idx} style={{backgroundColor: color}}></i>)
+                    })}
+                </div>
+            </Card.Footer>
         </Card>
     )
 }
